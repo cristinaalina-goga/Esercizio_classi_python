ca59b6498f3d2e847e0ea31de0ac0570afe8289c
Andrea <bosc.andrea.03@gmail.com>
2024-10-30 16:12:44 +0100

    scheletro fatto meglio

---
+++ my_scrypt_1.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as sopt 
+
+nomefile='nome_del_tuo_file'
+
+
+#...........
+#io faccio classe
+#salvo il dataframe in 'dati'
+#...........
+
+
+
+
+#........
+#fai grafico con colormap, usa pd.cut e aggiungi una colonna che chiami 'età' con il numero di intervallo al dataframe 'dati' 
+#........
+
+#...
+#faccio istogramma
+#...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
