c75c68a9f680cbe60ec3948346f46710e2e87f75
Andrea <bosc.andrea.03@gmail.com>
2024-10-30 15:15:34 +0100

    scheletro

--- untitled1.py
+++ untitled1.py
@@ -11,37 +11,20 @@ from matplotlib import pyplot as plt
 import matplotlib.colors as mcolors
 
 
-bins=10
+...............
+io creo la classe
+...........................
 
-binsarr=np.linspace(0,bins-1,bins)
-
-dati=pd.read_csv('HRdati.dat', sep=' ')
-
-
-
-plt.scatter(dati['b-y'],dati['M_ass'],s=1)
-plt.gca().invert_yaxis()
-
-numero=np.linspace(0,1,bins)
-
-distanza=(dati['age_parent'].max()-dati['age_parent'].min())/bins
-
-dati['età']=pd.cut(dati['age_parent'],bins=bins,labels=numero)
-
-norm = mcolors.Normalize(vmin=dati['age_parent'].min(), vmax=dati['age_parent'].max())
-
-plt.scatter(dati['b-y'],dati['M_ass'],s=1,c=dati['età'],cmap='plasma')
-
-plt.legend( [f'{i}-{i+np.round(distanza,3)}' for i in np.round(binsarr*distanza,1)], title="Gruppi di Età",loc='upper right')
-
-
-plt.show()
-
-
-dati.groupby('età')['M_ass'].hist(alpha=0.5)
-
-plt.legend( [f'{i}-{i+np.round(distanza,3)}' for i in np.round(binsarr*distanza,1)], title="Gruppi di Età",loc='upper right')
+#chiamerò il dataframe 'dati'  colonna 1='M_ass' ,colonna  2='b-y', colonna 3='age_parent'
 
 
 
-print(dati['età'])
+............................
+crea grafico con colormap
+...........................
+
+#dividi in 10 intervalli che chiamerai bins
+
+......................
+faccio isogramma
+......................
